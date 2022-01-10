61716be1bcc1f6e2672cccac7d507c5ccc78a0bb
Wilkenson Hilarion <wilkenson@Wilkensons-MBP.home>
2022-01-09 21:34:29 -0500

    modern js

---
+++ Desktop/javaScriptCourses/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,25 @@
+// export
+
+console.log('Exporting module');
+
+// /Blocking code
+
+// console.log('start');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+
+// console.log('finish fetching');
+/*
+const shippingCost = 10;
+
+export const cart = [];
+
+export const addToCart = function (product, qty) {
+  cart.push({ product, qty });
+  console.log(`${product} ${qty}`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
+*/
